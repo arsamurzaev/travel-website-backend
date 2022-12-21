9be295397d5e29c08f0c76d7d8e259b3690e4134
ridvan-yandaev <jorikridvan@mail.ru>
2022-12-21 19:29:45 +0300

    sdelal multer

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ const router = Router();
 
 router.use("/hotels", require("./hotels.route"));
 router.use(require("./users.route"));
+router.use(require('./organizations.route'))
 
 module.exports = router;
